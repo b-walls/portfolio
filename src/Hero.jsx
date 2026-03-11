d505bbe737b0dec76bd113e380692ac8d1478e19
Brendan Walls <wallsbr@dukes.jmu.edu>
2026-03-11 17:39:52 -0400

    Hero effects

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,16 +1,55 @@
-import React from 'react'
+import { useEffect, useRef } from "react";
 import './style/Hero.css'
+import Tilt  from 'react-parallax-tilt'
+import Typed from 'typed.js'
 
 function Hero() {
+  // const typed = new Typed('#status-typed', {
+  //   strings: ["First sentence.", "Second sentence.", "Third sentence."],
+  //   typeSpeed: 50,
+  //   backSpeed: 30,
+  //   backDelay: 1500,
+  // });
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "Open to Software Engineering Roles",
+        "Building Full-Stack Applications",
+        "Designing Backend Systems",
+        "About to graduate!",
+      ],
+      typeSpeed: 55,
+      backSpeed: 35,
+      backDelay: 3000,
+      loop: true,
+      showCursor: true,
+      cursorChar: "|",
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="hero" id="hero">
         <div className="hero-container">
           <div className="left-hero">
             <h2 className="oswald-500 profession">FULL-STACK DEVELOPER</h2>
             <h1 className="hero-name barrio-regular">Brendan<br></br> Walls</h1>
-            <p className="oswald-300">Hello, I'm Brendan! I'm a full-stack developer from Richmond, VA. </p>
+            <p className="oswald-300"></p>
           </div>
           <div className="right-hero">
+            <Tilt
+              tiltMaxAngleX={4}
+              tiltMaxAngleY={4}
+              perspective={1200}
+              transitionSpeed={5000}
+              scale={1.03}
+              gyroscope={true}
+            >
               <div className="computer">
                 <div className="window-controls">
                   <span></span>
@@ -23,7 +62,8 @@ function Hero() {
 
                     {'    '}<span className="property">name</span>: <span className="string">"Brendan Walls"</span>,<br/>
                     {'    '}<span className="property">title</span>: <span className="string">"Full-Stack Developer"</span>,<br/>
-
+                    {'    '}<span className="property">status</span>: <span className="string">"</span>
+                    <span className='string' ref={el}></span><span className="string">"</span><br/>
                     {'    '}<span className="property">education</span>: <span className="brace">{'{'}</span><br/>
                     {'        '}<span className="property">school</span>: <span className="string">"James Madison University"</span>,<br/>
                     {'        '}<span className="property">degree</span>: <span className="string">"Bachelor's of Science"</span>,<br/>
@@ -45,23 +85,11 @@ function Hero() {
                     <span className="string">"CSS"</span>
                     <span className="bracket">]</span>,<br/>
 
-                    {'    '}<span className="property">frameworks</span>: <span className="bracket">[</span>
-                    <span className="string">"Django"</span>,
-                    <span className="string">"React"</span>
-                    <span className="bracket">]</span>,<br/>
-
-                    {'    '}<span className="property">data</span>: <span className="bracket">[</span>
-                    <span className="string">"SQL"</span>,
-                    <span className="string">"Data Analytics"</span>,
-                    <span className="string">"Machine Learning"</span>
-                    <span className="bracket">]</span>,<br/>
-
-                    {'    '}<span className="property">status</span>: <span className="string">"Open to Software Engineering Roles"</span><br/>
-
                     <span className="brace">{'}'}</span>
                   </code>
                 </pre>
               </div>
+            </Tilt>
           </div>
         </div>
     </div>
